dd64692a4d8feadaa4dd51edc896e26b1e1c5f01
Raju Rangaswami <rangaswami@gmail.com>
2022-02-10 16:38:37 -0500

    Elevator thread stubs

--- nachos-3.4/code/threads/elevator.cc
+++ nachos-3.4/code/threads/elevator.cc
@@ -1,17 +1,51 @@
 #include "copyright.h"
 #include "system.h"
 
+
+
+typedef struct Person {
+    int id;
+    int atFloor;
+    int toFloor;
+} Person;
+
+void ElevatorThread(int numFloors) {
+    printf("Elevator with %d floors was created!\n", numFloors);
+}
+
+
+
 void Elevator(int numFloors) {
+    // Create Elevator Thread
+    Thread *t = new Thread("Elevator");
+    t->Fork(ElevatorThread, numFloors);
 
-    // Fill this out
-    printf("Elevator function invoked!\n");
+}
 
+void PersonThread(int person) {
+
+    Person *p = (Person *)person;
+
+    printf("Person %d wants to go from floor %d to %d\n", p->id, p->atFloor, p->toFloor);
+
+}
+
+int getNextPersonID() {
+    return 0;
 }
 
 
 void ArrivingGoingFromTo(int atFloor, int toFloor) {
 
-    // Fill this out
-    printf("Person wants to go from floor %d to %d\n", atFloor, toFloor);
+
+    // Create Person struct
+    Person *p = new Person;
+    p->id = getNextPersonID();
+    p->atFloor = atFloor;
+    p->toFloor = toFloor;
+
+    // Creates Person Thread
+    Thread *t = new Thread("Person " + p->id);
+    t->Fork(PersonThread, (int)p);
 
 }
